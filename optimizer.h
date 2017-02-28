76cdbc044dc8e677df216fbafff6bf4a5080e070
Jesús Mejía <jesusmejded@gmail.com>
2017-02-27 21:07:16 -0600

    Cambio en operador de mezcla

--- optimizer.h
+++ optimizer.h
@@ -38,8 +38,6 @@ void mixer(Chessboard* parent1, Chessboard* parent2, Chessboard* son){
 	}
 
 	k = 0;
-
-	i = 0; j = 0;
 	for (i= 0; i < N; ++i) {
 		for (j = 0; j < N; ++j) {
 			if (!parent2->matrix[i][j])
@@ -47,18 +45,20 @@ void mixer(Chessboard* parent1, Chessboard* parent2, Chessboard* son){
 
 			// Si el padre1 pierde el volado
 			// el padre2 hereda el bit
-			if (!flag1 && k < N && son->matrix[i][j]){
+			if (!son->matrix[bits[k][0]][bits[k][1]] && randm() < 0.5 && k < N){
 				son->matrix[bits[k][0]][bits[k][1]] = 1;
 				flag1 = 1;
-			}else{
+				k++;
+			}else if (!son->matrix[i][j]){
 				son->matrix[i][j] = 1;
 				flag1 = 0;
+				k++;
 			}
 			
-			k++;
 
 		}
 	}
+
 
 	while (k < N){
 			i = (int) (randm()*N);
@@ -70,20 +70,20 @@ void mixer(Chessboard* parent1, Chessboard* parent2, Chessboard* son){
 	}	
 }
 
-void mutation(short matrix[N][N]){
+void mutation(Chessboard* solution){
 	int i, j;
 	i = (int) (randm() * N);
 	j = (int) (randm() * N);
 
-	short v = matrix[i][j];
-	matrix[i][j] = !v; 
+	short v = solution->matrix[i][j];
+	solution->matrix[i][j] = !v; 
 
 	while(1) {
 		i = (int) (randm() * N);
 		j = (int) (randm() * N);
 
-		if (matrix[i][j] != v){
-			matrix[i][j] = v;
+		if (solution->matrix[i][j] != v){
+			solution->matrix[i][j] = v;
 			return;
 		}
 
@@ -93,42 +93,48 @@ void mutation(short matrix[N][N]){
 
 Chessboard crossing(int pop_size, Chessboard solutions[], float pmutation){
 	int i,j, minn = 0, maxx = 0, tmp;
-	int order[pop_size]; order[0] = 0;
+	int order[pop_size];
 	int parent[pop_size], parent_len = 0;
 	float cf;
 
-	// buscamos los valores de aptutud
-	// máximos y mínimos
-	for (i = 1; i < pop_size; ++i) {
+	// Ordenamiento para remplazo de padres por hijos 
+	for (int i = 0; i < pop_size; ++i)
 		order[i] = i;
-		if (solutions[maxx].val < solutions[i].val)
-			maxx = i;
-		
-		if (solutions[minn].val > solutions[i].val)
-			minn = i;
+
+	for (i = 0; i < pop_size - 1; ++i) {
+		for (j = pop_size - 1; j > i; --j) {
+			if (solutions[order[j - 1]].val < solutions[order[j]].val){
+				int tmp = order[j];
+				order[j] = order[j - 1];
+				order[j-1] = tmp;
+			}
+		}
 	}
 
 
 	// Ruleta para elegir a los padres
 	for (i = 0; i < pop_size; ++i) {
-		cf = 1 - crossingFactor(solutions[i].val,
-							solutions[minn].val,
-							solutions[maxx].val);
+		cf = (float) (order[i]) / (float) (pop_size);
 
-		if (randm() <= cf)
-			parent[parent_len++] = i;
+		if (randm() <= cf){
+			parent[parent_len++] = order[i];
+		}
 		else
-			tmp = i;
+			tmp = order[i];
 	}
 
 	// Si la cantidad de padres es impar
 	// se agrega el último padre que perdió
 	// la ruleta
 	if (parent_len == 0 || parent_len % 2 != 0)
-		if (parent_len < 1)
+		if (parent_len < 1){
 			parent[parent_len++] = tmp;
+			parent[parent_len++] = order[pop_size - 1];
+		}
 		else if (parent_len >= pop_size)
 			parent_len = pop_size;
+		else
+			parent[parent_len++] = tmp;
 
 
 	Chessboard* children = (Chessboard *) malloc(sizeof(Chessboard) * parent_len / 2 );
@@ -140,28 +146,27 @@ Chessboard crossing(int pop_size, Chessboard solutions[], float pmutation){
 			  &solutions[parent[2*i+1]],
 			  &children[i]);
 
+
+		// printf("========== padres   =====\n");
+		// showChessboard(solutions[parent[2*i]]);
+		// showChessboard(solutions[parent[2*i + 1]]);
+		// printf("========== hijo   =====\n");
+		// showChessboard(children[i]);
+
 		if (randm() <= pmutation) {
-			mutation(children[i].matrix);
+			// printf("mutation\n");
+			mutation(&children[i]);
 		}
 
 
 		children[i].val = fitnessFunction(children[i]);
 	}
 
-
-	// Ordenamiento para remplazo de padres por hijos 
-	for (i = 0; i < pop_size - 1; ++i) {
-		for (j = pop_size - 1; j > i; --j) {
-			if (solutions[order[j - 1]].val < solutions[order[j]].val){
-				int tmp = order[j];
-				order[j] = order[j - 1];
-				order[j-1] = tmp;
-			}
+	j = 0;
+	for (i = 0; i < parent_len / 2; ++i){
+		if (solutions[order[j]].val >= children[i].val) {
+			solutions[order[j++]] = children[i];
 		}
-	}
-
-	for (i = 0; i < parent_len / 2; ++i) {
-		solutions[order[i]] = children[i];
 	}
 
 	for (i = 0; i < pop_size ; ++i) {
